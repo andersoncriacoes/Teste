afe8c22a91d6a4e9ccecb2e98d682e03fa32ab91
andersoncriacoes <anderson0a0c@yahoo.com.br>
2021-01-23 13:29:18 -0200

    Teste com IF

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
-<html>
+<!doctype = html>
+    <html lang="pt-br">
     <head>
         <title>Título</title>
+        <meta charset="utf-8" />
     </head>
     <body>
-        <h1>
-            teste
-        </h1>
-        
+        <?php
+            $idade = 18;
+            
+            if($idade >= 18){
+                echo "<h1 style='color:green'> Autorizado </h1>";
+            } else {
+                echo "<h1 style='color:red'> Negado </h1>";    
+            }
+        ?>
     </body>
 </html>
